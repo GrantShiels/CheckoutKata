b30d10684d4da80fec0a72871af0f918b13e5068
grant <grant.dev@outlook.com>
2022-07-03 18:30:59 +0100

    Loaded Shop into main program
    Created a new Shop within the porgram.cs and loaded in the default items from the word document

--- CheckoutKata_App/Program.cs
+++ CheckoutKata_App/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using CheckoutKata_App.Models;
 
+namespace KataConsole
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            //Create a new Shop item on start
+            Shop currentShop = new Shop();
+
+            //Load in the default shop items from the word document
+            currentShop.ShopItems.Add(new ShopItem('A', 10));
+            currentShop.ShopItems.Add(new ShopItem('B', 15));
+            currentShop.ShopItems.Add(new ShopItem('C', 40));
+            currentShop.ShopItems.Add(new ShopItem('D', 55));
+
+            //display all the items to the user
+            foreach(var i in currentShop.ShopItems)
+            {
+                Console.WriteLine(
+                           "Item SKU: "
+                               + i.ItemSKU
+                               + " | Unit Price: "
+                               + i.UnitPrice
+                       );
+            }
+
+
+
+
+        }
+
+
+    }
+}
