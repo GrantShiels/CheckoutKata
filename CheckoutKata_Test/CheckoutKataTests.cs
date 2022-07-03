6a47752e7907b3b83c927554d4c66c7b2659d82d
grant <grant.dev@outlook.com>
2022-07-03 19:50:29 +0100

    Created default shop in test
    Created a default shop that mimics the existing one from word document. Will use this as a base to test the results from the total calculations

--- CheckoutKata_Test/CheckoutKataTests.cs
+++ CheckoutKata_Test/CheckoutKataTests.cs
@@ -1,10 +1,37 @@
-﻿namespace CheckoutKata_Test;
+﻿using CheckoutKata_App.Models;
+
+namespace CheckoutKata_Test;
 
 public class CheckoutKataTests
 {
-    [Fact]
-    public void Test1()
+    private Shop testShop = new Shop();
+
+    internal void KataConsoleTests()
     {
 
+        List<ShopItem> testShopItems = new List<ShopItem>();
+        List<ShopPromotion> testShopPromotions = new List<ShopPromotion>();
+
+
+        testShopItems.Add(new ShopItem('A', 10));
+        testShopItems.Add(new ShopItem('B', 15));
+        testShopItems.Add(new ShopItem('C', 40));
+        testShopItems.Add(new ShopItem('D', 55));
+
+        testShopPromotions.Add(new ShopPromotion('B', 3, 5, "3 for 40"));
+        testShopPromotions.Add(
+            new ShopPromotion('D', 2, 27.50M, "25% off for every 2 purchased together")
+        );
+
+
+        testShop.ShopItems = testShopItems;
+        testShop.ShopPromotions = testShopPromotions;
+        testShop.UserBasket = new List<ShopItem>();
     }
+
+
+
+
+
+
 }
