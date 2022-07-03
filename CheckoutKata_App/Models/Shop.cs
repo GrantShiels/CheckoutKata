589ffe0a051ce44c8796a70b0b95475f260ea1ac
grant <grant.dev@outlook.com>
2022-07-03 18:24:07 +0100

    Created basic models for items and shop
    ShopItem will contain the parameters for each item a user can buy.
    Shop will contain the list of all the items that the user can buy and all the items the user currently has within their own basket.

---
+++ CheckoutKata_App/Models/Shop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CheckoutKata_App.Models
+{
+    public class Shop
+    {
+
+
+        public List<ShopItem> ShopItems { get; set; }
+        public List<ShopItem> UserBasket { get; set; }
+
+
+        public Shop()
+        {
+            ShopItems = new List<ShopItem>();
+            UserBasket = new List<ShopItem>();
+        }
+    }
+}
+
---
+++ CheckoutKata_App/Models/ShopItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace CheckoutKata_App.Models
+{
+    public class ShopItem
+    {
+        //Base properties of every item
+        public char ItemSKU { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        //Create new item from supplied parameters
+        public ShopItem(char newSKU, decimal newPrice)
+        {
+            ItemSKU = newSKU;
+            UnitPrice = newPrice;
+        }
+
+   
+    }
+}
+
+
+
