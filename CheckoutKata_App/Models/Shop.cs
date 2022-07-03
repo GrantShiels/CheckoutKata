d9eb7d9e926d56e9146519f4bdc03bee9b3d00b3
grant <grant.dev@outlook.com>
2022-07-03 18:37:21 +0100

    Created ShopPromotion and added it to Shop.cs
    This class will be used to store and promotions that the shop currently provides

--- CheckoutKata_App/Models/Shop.cs
+++ CheckoutKata_App/Models/Shop.cs
@@ -7,12 +7,14 @@ namespace CheckoutKata_App.Models
 
         public List<ShopItem> ShopItems { get; set; }
         public List<ShopItem> UserBasket { get; set; }
+        public List<ShopPromotion> ShopPromotions { get; set; }
 
 
         public Shop()
         {
             ShopItems = new List<ShopItem>();
             UserBasket = new List<ShopItem>();
+            ShopPromotions = new List<ShopPromotion>();
         }
     }
 }
---
+++ CheckoutKata_App/Models/ShopPromotion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace CheckoutKata_App.Models
+{   
+    public class ShopPromotion
+    {
+        //Base properties of every promotion
+        public char ItemSKU { get; set; }
+        public int ItemCount { get; set; }
+        public decimal PriceSaving { get; set; }
+        public string PromotionText { get; set; }
+
+        public ShopPromotion(
+            char newSKU,
+            int newCount,
+            decimal newPriceSaving,
+            string newPromotionText
+        )
+        {
+            ItemSKU = newSKU;
+            ItemCount = newCount;
+            PriceSaving = newPriceSaving;
+            PromotionText = newPromotionText;
+        }
+    }
+    
+
+}
+
